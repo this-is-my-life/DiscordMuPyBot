d44f8c7a603aa51b5b500cfddee67a4c6f7853e6
PMHStudio <kok4575@gmail.com>
2018-11-03 23:46:20 +0900

    [11/03] Fire in the hole!

---
+++ CORE.py
@@ -0,0 +1,12 @@
+import discord
+from discord.ext import commands
+
+muto = System.getenv("muto")
+
+mu = commnads.Bot(command_prefix = 'mp!')
+
+@mu.event
+async def on_ready():
+	print('MuPy is Here');
+
+mu.run(muto)

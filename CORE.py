d8aa2122a2f3587327f90b16b389972b4a6de4f6
PMHStudio <kok4575@gmail.com>
2018-11-04 09:05:07 +0900

    [11/4] Added Comment

--- CORE.py
+++ CORE.py
@@ -1,13 +1,39 @@
-import os
-import discord
-from discord.ext import commands
+'''
+	μPy! v6.0 Core. 
+	---------------------
+	PMH Studio / Proj- μBot | Smart & Cute Discord Bot_Mu~☆ 
+	Copyright (c) 2018. PMH Studio / PMH. (kok4575@gmail.com) MIT Licensed.
+	
+	* Requests Node.js & Discord.js
+'''
 
-muto = os.getenv("muto")
+# Basic Imports__________________________________
 
-mu = commands.Bot(command_prefix = 'mp!')
+	# Get Environment Variable System Import
+	import os
 
-@mu.event
-async def on_ready():
-	print('MuPy is Here');
+	# Discord API Import
+	import discord
 
-mu.run(muto)
+	# Discord API Commands Import
+	from discord.ext import commands
+
+# Get Token______________________________________
+	
+	# Get Token From Enviroment Variable or Text
+	muto = os.getenv("muto") || ' Token Here. Or Get Environment Variable '
+
+# Bot Login______________________________________
+
+	# Get Bot Client
+	mu = commands.Bot(command_prefix = 'mp!')
+
+	# Bot Login with Token
+	mu.run(muto)
+
+# Bot Readying___________________________________
+	@mu.event
+	async def on_ready():
+		print('MuPy is Here');
+
+	

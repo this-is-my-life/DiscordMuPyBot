4ce6e557ca503421af94011983e67ffde89a3f9e
PMHStudio <kok4575@gmail.com>
2018-11-04 09:06:59 +0900

    [11/04] IndentationError: unexpected indent

--- CORE.py
+++ CORE.py
@@ -9,31 +9,31 @@
 
 # Basic Imports__________________________________
 
-	# Get Environment Variable System Import
-	import os
+# Get Environment Variable System Import
+import os
 
-	# Discord API Import
-	import discord
+# Discord API Import
+import discord
 
-	# Discord API Commands Import
-	from discord.ext import commands
+# Discord API Commands Import
+from discord.ext import commands
 
 # Get Token______________________________________
 	
-	# Get Token From Enviroment Variable or Text
-	muto = os.getenv("muto") || ' Token Here. Or Get Environment Variable '
+# Get Token From Enviroment Variable or Text
+muto = os.getenv("muto") || ' Token Here. Or Get Environment Variable '
 
 # Bot Login______________________________________
 
-	# Get Bot Client
-	mu = commands.Bot(command_prefix = 'mp!')
+# Get Bot Client
+mu = commands.Bot(command_prefix = 'mp!')
 
-	# Bot Login with Token
-	mu.run(muto)
+# Bot Login with Token
+mu.run(muto)
 
 # Bot Readying___________________________________
-	@mu.event
-	async def on_ready():
-		print('MuPy is Here');
+@mu.event
+async def on_ready():
+	print('MuPy is Here');
 
 	

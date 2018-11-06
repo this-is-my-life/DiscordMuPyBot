b7b10cbbaba659c7df9303aa86bd190e2283da18
PMHStudio <kok4575@gmail.com>
2018-11-06 19:57:54 +0900

    [11/06] Python MuBot + Js MuBot

--- CORE.py
+++ CORE.py
@@ -32,10 +32,10 @@ muto = os.getenv("muto") or ' Token Here. Or Get Environment Variable '
 # Bot Login______________________________________
 
 # Get Bot Client
-mu = commands.Bot(command_prefix = 'mp!')
+mu = commands.Bot(command_prefix = 'mu!')
 
 # Bot Status Cycle
-status = ['제작: PMH Studio / PMH', 'Python 3.6 & Discord.py기반', 'MuPy! (MuBot Python Version)', '일단 테스트중! 띠꺼우면 PMH에게!', '오프은 소스으 (깃헙참고)', 'github.com/PMHStudio/DiscordMuPyBot', 'mubotapi.dothome.co.kr/', 'pmhstudio.co.nf']
+status = ['Type mu!help to HELP', 'Discord.js & Discord.py', 'Python + Node.js Version', '일단 테스트중! 띠꺼우면 PMH에게!', 'Open Source', 'github.com/PMHStudio/', 'mubotapi.dothome.co.kr/', 'pmhstudio.co.nf/', 'Created By PMH Studio', '에에 심심하다뮤! | mu!(하고싶은말)']
 async def change_status():
 	await mu.wait_until_ready()
 	msgs = cycle(status)
@@ -61,51 +61,38 @@ async def on_message(input):
 	ichannel = input.channel
 	iserver = input.server
 	
-	if i.startswith('mp!') or i.startswith('뮤파!'):
+	if i.startswith('mu!') or i.startswith('뮤!'):
 
 		# Logging
 		print('{}/{}> {}'.format(ichannel, iuser, i))
 
-		if i.startswith('mp!ping') or i.startswith('mp!p'):
-			await mu.send_message(ichannel, 'Pong')
-
-		if i.startswith('뮤파!핑') or i.startswith('뮤파!핑크'):
-			await mu.send_message(ichannel, '퐁!')
-
-		if i.startswith('mp!say') or i.startswith('mp!s') or i.startswith('뮤파!말') or i.startswith('뮤파!말해줘'):
-			output = ''
-			for word in itab[1:]:
-				output += word
-				output += ' '
-			await mu.send_message(ichannel, output)
-
-		if i.startswith('mp!delete') or i.startswith('mp!d'):
-			amount = 99
+		if i.startswith('mu!delete') or i.startswith('mu!d'):
+			amount = 1
 			for a in itab[1:]:
 				amount = a
 			if int(amount) > 99:
-				await mu.send_message(ichannel, 'MuPy Can Delete Less than 99 Message!')
+				await mu.send_message(ichannel, 'Mu Can Delete Less than 99 Message!')
 			else:
 				deletes = []
 				async for input in mu.logs_from(ichannel, limit=int(amount) + 1):
 					deletes.append(input)
 				await mu.delete_messages(deletes)
-				await mu.send_message(ichannel, 'Deleted!')
+				await mu.send_message(ichannel, 'Deleted {} Messages By \'{}\'!'.format(amount, iuser))
 
-		if i.startswith('뮤파!삭') or i.startswith('뮤파!삭제'):
-			amount = 99
+		if i.startswith('뮤!삭') or i.startswith('뮤!삭제'):
+			amount = 1
 			for a in itab[1:]:
 				amount = a
 			if int(amount) > 99:
-				await mu.send_message(ichannel, '뮤파이는 99개의 메세지까지 지울 수 있다뮤!')
+				await mu.send_message(ichannel, '뮤는 99개의 메세지까지 지울 수 있다뮤!')
 			else:
 				deletes = []
 				async for input in mu.logs_from(ichannel, limit=int(amount) + 1):
 					deletes.append(input)
 				await mu.delete_messages(deletes)
-				await mu.send_message(ichannel, '삭제 완료!')
+				await mu.send_message(ichannel, '\'{}\'의 요청으로\n{}개의 메세지 삭제 완료!'.format(iuser, amount))
 
-		if i.startswith('뮤파!닉'):
+		if i.startswith('뮤!닉'):
 			output = ''
 			for word in itab[1:]:
 				output += word
